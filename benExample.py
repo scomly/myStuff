9a1cd9856aeb541ba45cf595424081a5d1b61b12
scomly <shauncomly@gmail.com>
2015-07-27 18:30:47 -0700

    Update benExample.py

--- benExample.py
+++ benExample.py
@@ -32,3 +32,49 @@ class Dialog(QtGui.QDialog):
 
 diag = Dialog()
 diag.show()
+
+def doOption1():
+    print "Doing Option1"
+
+def doOption2():
+    print "Doing Option2"
+
+class MyCheckBox(QtGui.QCheckBox):
+    def __init__(self, parent, name):
+        super(MyCheckBox, self).__init__(parent)
+        self.setParent(parent)
+        self.setFont(QtGui.QFont("Arial"))
+        self.setText(name)
+
+class MyDialog(QtGui.QDialog):
+    def __init__(self):
+        super(MyDialog, self).__init__()
+        layout = QtGui.QHBoxLayout()
+        self.setLayout(layout)
+
+        goButton = QtGui.QPushButton("Go")
+        layout.addWidget(goButton)
+        goButton.clicked.connect(self.runOptionFunctions)
+
+        self.allOptionsDict = {}
+        optionDict = { "option1": doOption1, "options2": doOption2 }
+
+        for option, optionFunction in optionDict.iteritems():
+            checkBox = MyCheckBox(self, option)
+            layout.addWidget(checkBox)
+            valueDict = { "widget": checkBox, "func": optionFunction }
+            self.allOptionsDict[option] = valueDict
+
+    def runOptionFunctions(self):
+        for option, value in self.allOptionsDict.iteritems():
+            widget = value["widget"]
+            function = value["func"]
+            if widget.isChecked():
+                function()
+
+diag = MyDialog()
+diag.show()
+
+
+
+
