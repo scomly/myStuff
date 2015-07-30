a588d0fe6e339626458b3bcc9d4cf40f324fb574
scomly <shauncomly@gmail.com>
2015-07-29 18:34:27 -0700

    Update yamlStuffHome.py

--- yamlStuffHome.py
+++ yamlStuffHome.py
@@ -13,13 +13,20 @@ def getMayaWindow():
 
 class UtilityToolBoxUI(QtGui.QDialog):
     
+
+    
     def __init__(self, parent=getMayaWindow()):
         super(UtilityToolBoxUI, self).__init__(parent)
         
         self.setWindowTitle("Utility Toolbox")
         self.setWindowFlags(QtCore.Qt.Tool) # makes it a tool window so it will stay on top of maya
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose) # deletes UI when closed
-        
+                
+        self.topList = ["Red", "Green", "Blue", "White", "Black"]
+        self.middleTopList = ["Shadow", "Contact_Shadow", "Fresnel", "Reflection_Occ"]
+        self.middleBotList = ["Shadow_Catcher", "Plate_Projection", "Reflection_Catcher"]
+        self.bottomList = ["Ref_Spheres"]
+
         self.createLayout() # runs def createLayout
         
     def createLayout(self):
@@ -33,6 +40,37 @@ class UtilityToolBoxUI(QtGui.QDialog):
         self.cbButtonList = {}
         self.getState = {}
         
+        #################################################################
+        
+        radioLayout = QtGui.QHBoxLayout()
+        layout.addLayout(radioLayout)
+        
+        spacer = QtGui.QSpacerItem(275,0)
+        radioLayout.addSpacerItem(spacer)
+        
+        radioGroup = QtGui.QButtonGroup()
+       
+        showRadio = QtGui.QRadioButton("Show")
+        showRadio.setMaximumWidth(50)
+        showRadio.setMinimumWidth(50)
+        showRadio.setChecked(True)
+        radioGroup.addButton(showRadio)
+        seqRadio = QtGui.QRadioButton("Seq")
+        seqRadio.setMaximumWidth(50)
+        seqRadio.setMinimumWidth(50)
+        radioGroup.addButton(seqRadio)
+        shotRadio = QtGui.QRadioButton("Shot")
+        shotRadio.setMaximumWidth(50)
+        shotRadio.setMinimumWidth(50)        
+        radioGroup.addButton(shotRadio)
+        
+        radioLayout.addWidget(showRadio)
+        radioLayout.addWidget(seqRadio)
+        radioLayout.addWidget(shotRadio)
+        
+        spacer2 = QtGui.QSpacerItem(250,0)
+        radioLayout.addSpacerItem(spacer2)
+        
         #################### top frame ##############################################
         
         self.top_frame = QtGui.QFrame()
@@ -45,18 +83,17 @@ class UtilityToolBoxUI(QtGui.QDialog):
         
         tl.frameLabelVarName.mouseReleaseEvent = self.topToggle
         
-        topList = ["Red", "Green", "Blue", "White", "Black"]
-        
+       
         self.topListCheckBox = {}
         
-        for x in topList:
+        for x in self.topList:
             cb = UtilCreateCheckBox(x, x, self.top_frame)
             self.topListCheckBox[x] = cb.buttonVarName
+            setattr(self, x, cb)
             
         self.cbButtonList.update(self.topListCheckBox)
         
         ####################### middle top frame #################################
-        
         self.middleTop_frame = QtGui.QFrame()
         self.middleTop_frame.setFrameStyle(QtGui.QFrame.Panel | QtGui.QFrame.Raised)
         layout.addWidget(self.middleTop_frame)
@@ -67,13 +104,13 @@ class UtilityToolBoxUI(QtGui.QDialog):
         
         mtl.frameLabelVarName.mouseReleaseEvent = self.midTopToggle
         
-        middleTopList = ["Shadow", "Contact_Shadow", "Fresnel", "Reflection_Occ"]
         
         self.midTopListCheckBox = {}
         
-        for x in middleTopList:
+        for x in self.middleTopList:
             cb = UtilCreateCheckBox(x, x, self.middleTop_frame)
             self.midTopListCheckBox[x] = cb.buttonVarName
+            setattr(self, x, cb)
             
         self.cbButtonList.update(self.midTopListCheckBox)
             
@@ -88,17 +125,17 @@ class UtilityToolBoxUI(QtGui.QDialog):
         mbl = FrameLabel("Shader_label", "Shaders", self.middleBot_frame)
         
         mbl.frameLabelVarName.mouseReleaseEvent = self.midBotToggle
-        
-        middleBotList = ["Shadow_Catcher", "Plate_Projection", "Reflection_Catcher"]
-        
+
         self.midBotListCheckBox = {}
         
-        for x in middleBotList:
-            cb = UtilCreateCheckBox(x, x, self.middleBot_frame)
+        for x in self.middleBotList:
+            cb = UtilCreateCheckBox(x, x, self.middleBot_frame) 
             self.midBotListCheckBox[x] = cb.buttonVarName
-       
+            setattr(self, x, cb)
+            
+            
+                       
         self.cbButtonList.update(self.midBotListCheckBox)
-             
         ############################ bottom frame ##########################################
         
         self.bottom_frame = QtGui.QFrame()
@@ -110,14 +147,14 @@ class UtilityToolBoxUI(QtGui.QDialog):
         bl = FrameLabel("Util_label", "Utilities", self.bottom_frame)
         
         bl.frameLabelVarName.mouseReleaseEvent = self.bottomToggle
-        
-        bottomList = ["Ref_Spheres"]
-        
+                
         self.bottomListCheckBox = {}
                
-        for x in bottomList:
+        for x in self.bottomList:
             cb = UtilCreateCheckBox(x, x, self.bottom_frame)
             self.bottomListCheckBox[x] = cb.buttonVarName
+            setattr(self, x, cb)
+            
             
         self.cbButtonList.update(self.bottomListCheckBox)     
                     
@@ -134,8 +171,6 @@ class UtilityToolBoxUI(QtGui.QDialog):
         self.checkNone_button = QtGui.QPushButton("Check None")
         allCheckLayout.layout().addWidget(self.checkNone_button)
         self.checkNone_button.clicked.connect(self.checkNoneFunction)
-                       
-        
         ####################### Import button #####################################################
         
         self.import_button = QtGui.QPushButton("Import")
@@ -155,11 +190,9 @@ class UtilityToolBoxUI(QtGui.QDialog):
         
         self.setLayout(layout) # add main layout itself to this dialog
         
-       
-    
+        
+        
     def checkAllFunction(self): ## Check all Button
-    
-        #getState = {}
         
         for x,y in self.cbButtonList.iteritems():
             
@@ -168,25 +201,27 @@ class UtilityToolBoxUI(QtGui.QDialog):
             else:
                 cbState = False
             self.getState[x] = cbState
-        print self.getState
-            
-        
-        with open('/Users/scomly/Desktop/yaml/test.yml', 'w') as outfile:
+                    
+        with open('/home/scomly/python/test_yaml/test.yml', 'w') as outfile:
             outfile.write(yaml.dump(self.getState, default_flow_style=False))
-    
-        #for x,y in self.cbButtonList.iteritems():
-            #y.setChecked(True)
+
         
     def checkNoneFunction(self): ## Check None Button
     
-        infile = open('/Users/scomly/Desktop/yaml/test.yml')
-        self.inSettings = yaml.load(infile)
+        infile = open('/home/scomly/python/test_yaml/test.yml')
+        inSettings = yaml.load(infile)
         infile.close()
-        #print self.inSettings
-        for x,y in self.inSettings.iteritems():
-            
 
+        for x in self.cbButtonList:
+            if x in inSettings.keys():
+                if inSettings[x]:
+                    exec('self.%s.buttonVarName.setCheckState(QtCore.Qt.Checked)' % (x))
+                else:
+                    exec('self.%s.buttonVarName.setCheckState(QtCore.Qt.Unchecked)' % (x))
+                
+                
 
+        
     def topToggle(self, event): ## Top list CB toggle
         flipRow(self.topListCheckBox)        
 
@@ -197,14 +232,8 @@ class UtilityToolBoxUI(QtGui.QDialog):
         flipRow(self.midBotListCheckBox)  
 
     def bottomToggle(self, event): ## Bottom list CB toggle
-        flipRow(self.bottomListCheckBox)             
+        flipRow(self.bottomListCheckBox)
         
-
-
-
-
-
-
 def flipRow(whichList): ## toggle row of checkboxes if you click on the label
     if whichList.values()[0].isChecked() == True:
         for x,y in whichList.iteritems():
@@ -222,24 +251,22 @@ class UtilCreateCheckBox(object):
         font.setPointSize(10)
         self.buttonVarName.setFont(font)
         self.buttonVarName.setChecked(True)
+        
 
         
 class FrameLabel(object):
     def __init__(self, frameLabelVarName, frameLabelName, frame):
         
-        #self.frameLabelVarName = frameLabelVarName
-        # frameLabelVarName = the frame creation variable name
         self.frameLabelName = frameLabelName
-        # frameLabelName = the name of the label on the frame
         self.frame = frame
-        # frame = which frame it will land in
         
         self.frameLabelVarName = QtGui.QLabel(frameLabelName)
         frame.layout().addWidget(self.frameLabelVarName)
         font = QtGui.QFont()
         font.setBold(True)
         font.setPointSize(15)
-        self.frameLabelVarName.setFont(font)       
+        self.frameLabelVarName.setFont(font)               
+        
         
 if __name__ == "__main__":
     
@@ -250,4 +277,6 @@ if __name__ == "__main__":
         pass
         
     ui = UtilityToolBoxUI()
-    ui.show()
+    ui.show()        
+        
+        
