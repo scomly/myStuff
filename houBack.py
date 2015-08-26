b7792d59ed4ef2a7deabf395f0be0572f528d3d4
scomly <shauncomly@gmail.com>
2015-08-25 21:52:21 -0700

    Update houBack.py

--- houBack.py
+++ houBack.py
@@ -9,6 +9,39 @@ sourceShader = hou.selectedNodes()[0]
 
 #shader.parent()
 
+import hou
+
+sourceShader = hou.selectedNodes()[0]
+#path = shader.path()
+#print shader
+
+#getShader = hou.node(path)
+
+#shader.parent()
+
+#ting = hou.node(path)
+
+sourceParms = sourceShader.parms()
+
+changedParms = {}
+
+for x in sourceParms:
+    isAt = x.isAtDefault()
+    if isAt == False:
+        parmname = x.name()
+        parmEval = x.eval()
+        changedParms[parmname] = parmEval
+        
+destinShader =  hou.selectedNodes()[0]
+#changeThis
+
+for x,y in changedParms.iteritems():
+   # print x
+   # print y
+   destinShader.parm(x).set(y)
+        
+
+
 #ting = hou.node(path)
 
 sourceParms = sourceShader.parms()
