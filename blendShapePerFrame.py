f47c067c533694d492b0bf12719a85c1b864331e
scomly <shauncomly@gmail.com>
2019-10-21 16:50:30 -0700

    Update blendShapePerFrame.py

--- blendShapePerFrame.py
+++ blendShapePerFrame.py
@@ -3,8 +3,18 @@ my_history = cmds.listHistory(my_mesh)
 blendShapes = cmds.ls(type='blendShape')
 
 for x in blendShapes:
-    blendShapeWeight = cmds.listAttr('%s.w' % x, m=True)   
+    blendShapeWeight = cmds.listAttr('%s.w' % x, m=True)
+    
+    getFrame = cmds.currentTime(q=True)
+    lastFrame = getFrame - 1
+    cmds.currentTime(lastFrame)
+    cmds.setKeyframe(x, at=blendShapeWeight, v=0)
+    
+    getFrame = cmds.currentTime(q=True)
+    nextFrame = getFrame + 1
+    cmds.currentTime(nextFrame)       
     cmds.setKeyframe(x, at=blendShapeWeight, v=1)
+    
     getFrame = cmds.currentTime(q=True)
     nextFrame = getFrame + 1
     cmds.currentTime(nextFrame)
