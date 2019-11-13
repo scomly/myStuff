c0b28e1be4099a195799ff7d2abf24218c158990
scomly <shauncomly@gmail.com>
2019-11-13 11:28:13 -0800

    Update nukeAutoCropAlpha_v001.py

--- nukeAutoCropAlpha_v001.py
+++ nukeAutoCropAlpha_v001.py
@@ -3,64 +3,75 @@ import time
 import csv
 import os
 
-#### Prints all knobs on selected node ####
-#### print( nuke.selectedNode() )
 
-#### Get Current Frame ####
-#### frame = nuke.frame()
-#### print frame
+#### Get multiple selected nodes ####
+selectList = nuke.selectedNodes()
 
-#### Get AutoCrop Data ####
-#### value = nuke.toNode('CurveTool2').knob('autocropdata').getValue()
-#### print value
+for x in selectList:
 
-#### Get name of selected read node ####
-getSelect = nuke.selectedNode()
-getName = getSelect['name'].value()
-#print getName
+    #### Get name of read node ####
+    getName = []
+    getName = x['name'].value()
+    #print getName
 
-#### Get Frame in and Out for selected read node ####
-getSelect = nuke.selectedNode()
-getFirstFrame = getSelect['first'].value()
-getLastFrame = getSelect['last'].value()
-#print getFirstFrame
-#print getLastFrame
+    #### Get frame in and out for selected read node ####
+    getFirstFrame = []
+    getLastFrame = []
+    getFirstFrame = x['first'].value()
+    getLastFrame = x['last'].value()
+    #print getFirstFrame
+    #print getLastFrame
 
-#### Creates and set Shuffle ####
-shuffle = nuke.nodes.Shuffle()
-shuffle['red'].setValue("alpha")
-shuffle['green'].setValue("alpha")
-shuffle['blue'].setValue("alpha")
+    #### Creates path to save CSV files based on file read path ####
+    getCurrentFilePath = []
+    getCurrentFilePath = x['file'].value()
+    filePathSplit = getCurrentFilePath.split('/')
+    updatedFilePath = getCurrentFilePath.replace(filePathSplit[-1],'')
+    #print updatedFilePath
 
-#### Connect shuffle to read node ####
-shuffle.connectInput(1,getSelect)
+    #### Get resolution for selected read node ####
+    getWdith = []
+    getHeight = []
+    scriptFormats = nuke.formats()
+    getWidth = x.width()
+    getHeight = x.height()
+    #print getWidth
+    #print getHeight
 
-#### Creates and Sets CurveTool ####
-curveTool = nuke.nodes.CurveTool()
-curveTool['operation'].setValue('Auto Crop')
-curveTool['color'].setValue(1)
-curveTool['ROI'].setValue([0,0,1920,1080])
-curveTool['afterFrameRender'].setValue("frameData[nuke.frame()]=nuke.toNode(getCurveToolLabel).knob('autocropdata').getValue()")
-getCurveToolLabel = curveTool['name'].value()
-#print getCurveToolLabel
+    #### Creates and sets shuffle ####
+    shuffle = []
+    shuffle = nuke.nodes.Shuffle()
+    shuffle['red'].setValue("red")
+    shuffle['green'].setValue("red")
+    shuffle['blue'].setValue("red")
 
-#### Connect curveTool to shuffle ####
-curveTool.connectInput(1,shuffle)
+    #### Connect shuffle to read node ####
+    shuffle.connectInput(1,x)
 
-#### Creates Empty Dictionary ####
-frameData = {}
+    #### Creates and sets curveTool ####
+    curveTool = []
+    curveTool = nuke.nodes.CurveTool()
+    curveTool['operation'].setValue('Auto Crop')
+    curveTool['ROI'].setValue([0,0,getWidth,getHeight])
+    curveTool['afterFrameRender'].setValue("frameData[nuke.frame()]=nuke.toNode(getCurveToolLabel).knob('autocropdata').getValue()")
+    getCurveToolLabel = curveTool['name'].value()
 
-#### Executes Curve Tool ####
-nuke.execute(curveTool,getFirstFrame,getLastFrame)
+    #### Connects curveTool to shuffle ####
+    curveTool.connectInput(1,shuffle)
 
-#### print frameData
+    #### Creates empty dictionary ####
+    frameData = {}
 
-#### Sets current working directory ####
-path = 'P:\\projects\\googlemeasure_36793P\\design\\work\\'
-os.chdir(path)
+    #### Executes curveTool ####
+    nuke.execute(curveTool,getFirstFrame,getLastFrame)
 
-#### Writes CSV file ####
-with open('frameData.csv','wb') as frameData_csv_file:
-    writer = csv.writer(frameData_csv_file)
-    for key, value in frameData.items():
-       writer.writerow([key, value])
+    #### Sets current working directory ####
+    path = updatedFilePath
+    os.chdir(path)
+
+    #### Writes CSV file ####
+    frameData_csv_file = []
+    with open('autoCropData.csv','wb') as frameData_csv_file:
+        writer = csv.writer(frameData_csv_file)
+        for key, value in frameData.items():
+           writer.writerow([key, value])
